720165330222806eba0ff239016e5f1a30169b19
impurity-dev <techkoko77@gmail.com>
2023-12-16 06:35:44 -0500

    Fix up

--- src/app/components/hero.tsx
+++ src/app/components/hero.tsx
@@ -1,7 +1,7 @@
 export default function Hero() {
     return (
         <div className="hero min-h-screen" style={{ backgroundImage: 'url(/images/white-flowers.jpg)' }}>
-            <div className="hero-overlay bg-opacity-60"></div>m "FixU"
+            <div className="hero-overlay bg-opacity-60"></div>
             <div className="hero-content text-center text-neutral-content backdrop-blur bg-white/20 rounded p-10">
                 <div className="max-w-md">
                     <h1 className="mb-5 text-5xl font-bold">Hello, gorgeous!</h1>
