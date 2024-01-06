a417a07ed5f6759082b4f84eddf6b960f5b236ec
impurity-dev <techkoko77@gmail.com>
2024-01-06 18:00:39 -0500

    Update and remove daisy

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,6 @@
 const config = {
     darkMode: 'class',
     content: ['./src/pages/**/*.{js,ts,jsx,tsx,mdx}', './src/components/**/*.{js,ts,jsx,tsx,mdx}', './src/app/**/*.{js,ts,jsx,tsx,mdx}'],
-    plugins: [require('@tailwindcss/typography'), require('daisyui')],
-    daisyui: {
-        themes: ['forest', 'retro'],
-    },
+    plugins: [require('@tailwindcss/typography')],
 };
 export default config;
